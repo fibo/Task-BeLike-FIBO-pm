410026493c93265e1b584ae4b0f277448a49d373
fibo <casati_gianluca@yahoo.it>
2015-05-15 10:36:31 -0500

    improved scaffolding script

---
+++ gh-pages/lib/My/Package.pm
@@ -0,0 +1,29 @@
+package My::Package;
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+My::Package --
+
+=begin HTML
+
+<p><a href="https://metacpan.org/pod/My::Package" target="_blank"><img alt="CPAN version" src="https://badge.fury.io/pl/My-Package.svg"></a> <a href="https://travis-ci.org/fibo/My-Package-pm" target="_blank"><img alt="Build Status" src="https://travis-ci.org/fibo/My-Package-pm.svg?branch=master"></a></p>
+
+=end HTML
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright © 2015 by L<G. Casati|http://g14n.info>.
+
+This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
+
+=cut
+
